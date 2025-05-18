5a8271e737926b8045c2d62df783d83f1c23b9e9
Adibab <adiba.du.bd@gmail.com>
2025-05-18 01:04:25 +0100

    removing unnecessary code

--- encode.py
+++ encode.py
@@ -6,37 +6,26 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-# def encrypt (original_text, shift_amount):
-#
-#
-#
-# # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-# #  by the shift amount and print the encrypted text.
-#
-# #get the index num of original text each letter so use index
-#  cipher_text =""
-#  for letter in original_text:
-#     shifted_position = alphabet.index(letter)+ shift_amount
-#
-#     shifted_position %= len(alphabet) # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-#     cipher_text += alphabet[shifted_position ]
-#  print(cipher_text)
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt (original_text, shift_amount):
+
+
+
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+
+#get the index num of original text each letter so use index
+ cipher_text =""
+ for letter in original_text:
+    shifted_position = alphabet.index(letter)+ shift_amount
+
+    shifted_position %= len(alphabet) # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+    cipher_text += alphabet[shifted_position ]
+ print(cipher_text)
 #
 #
 # # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 # #  message.
-#
-# encrypt(original_text=text, shift_amount=shift)
 
-# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
-def decrypt(original_text, shift_amount):
+ encrypt(original_text=text, shift_amount=shift)
 
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-    cipher_text = ""
-    for letter in original_text:
-        shifted_position = alphabet.index(letter) - shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the decoded result: {cipher_text}")
-decrypt(original_text=text, shift_amount=shift)
