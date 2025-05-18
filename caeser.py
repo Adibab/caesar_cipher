04ab8a3ccbc77613813054ca606a88e54131b08c
Adibab <adiba.du.bd@gmail.com>
2025-05-18 11:51:19 +0100

    encode and decode funtion together

--- caeser.py
+++ caeser.py
@@ -1,23 +1,30 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
 
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-def ceaser(original_text, shift_amount,encode_or_decode):
-    cipher_text = ""
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
 
     for letter in original_text:
 
-        if encode_or_decode == "decode":
-            shift_amount *= -1 #multiplying by -1 will make the shift_amount neg so same code can be used for both encode and decode
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the {encode_or_decode}d result: {cipher_text}")
+        if letter not in alphabet:
+            output_text+= letter
+        else:
+            if encode_or_decode == "decode":
+                shift_amount *= -1
+
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-ceaser(original_text=text, shift_amount=shift, encode_or_decode=direction)
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
